fc8ac5626cb2c472137cb596edadc809561d71f3
Nissan1998 <jnnissanm@gmail.com>
2023-05-04 04:03:02 +0600

    Authentication

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,8 +2,25 @@ import React, { useContext } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../PrivetRoute/AuthProvider";
+import { AiFillGoogleSquare } from "react-icons/ai";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../Firebase/Firebase.config";
 
 const Login = () => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    console.log("googleclicked");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        navigate(from, { replace: true });
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   const { signIn } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
@@ -37,9 +54,9 @@ const Login = () => {
     >
       <Container className="w-25 mx-auto">
         <h4 className="text-center text-white">Please Login</h4>
-        <Form onSubmit={handleLogin}>
+        <Form className="mb-2" onSubmit={handleLogin}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label className="text-black">Email address</Form.Label>
+            <Form.Label className="text-white">Email address</Form.Label>
             <Form.Control
               type="email"
               name="email"
@@ -49,7 +66,7 @@ const Login = () => {
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label className="text-black">Password</Form.Label>
+            <Form.Label className="text-white">Password</Form.Label>
             <Form.Control
               type="password"
               name="password"
@@ -62,12 +79,25 @@ const Login = () => {
             Login
           </Button>
           <br />
-          <Form.Text className="text-black">
+          <Form.Text className="text-white">
             Do not Have an Account? <Link to="/register">Register</Link>
           </Form.Text>
-          <Form.Text className="text-success"></Form.Text>
-          <Form.Text className="text-danger"></Form.Text>
         </Form>
+        <Form.Text className="text-white me-2 ">Continue With</Form.Text>
+        <Button onClick={handleGoogleSignIn} className="me-3" variant="light">
+          <img
+            style={{ width: 25 }}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/706px-Google_%22G%22_Logo.svg.png"
+            alt=""
+          />
+        </Button>
+        <Button variant="light">
+          <img
+            style={{ width: 25 }}
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt=""
+          />
+        </Button>
       </Container>
     </div>
   );
