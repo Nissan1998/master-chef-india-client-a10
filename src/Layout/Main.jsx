f54acab59ca5fc687a4295718d79ead2d5ffda66
Nissan1998 <jnnissanm@gmail.com>
2023-05-05 19:23:04 +0600

    titleSet

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,11 +3,10 @@ import Header from "../Pages/Shared/Header/Header";
 import Footer from "../Pages/Shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
-import Hero from "../Pages/HeroSection/Hero";
 
 const Main = () => {
   return (
-    <div className="">
+    <div>
       <Header></Header>
       <Row>
         <Col lg={12}>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider } from "react-router-dom";
